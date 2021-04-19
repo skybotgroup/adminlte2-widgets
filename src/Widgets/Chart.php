df68f0f28b57a7ec10d5797c262291186f196e0a
Maksym Sliusarchuk <max.slyusarchuk@gmail.com>
2021-04-19 22:12:27 +0300

    Extend chart widget. Add range configurations for ajax method

--- src/Widgets/Chart.php
+++ src/Widgets/Chart.php
@@ -18,6 +18,9 @@ class Chart extends Widget
     protected $data = [];
     protected $options = [];
     protected $ajax = null;
+    protected $range = false;
+    protected $ranges = null;
+    protected $defaultRange = null;
 
     /**
      * Chart constructor.
@@ -71,20 +74,25 @@ class Chart extends Widget
         $form = new Form();
         if ($this->ajax !== null)
         {
-            $form->daterangepicker(["$this->id-start", "$this->id-end"], __('Period'))
-                ->ranges([
-                    __('Today') => [Carbon::today()->toDateString(), Carbon::today()->toDateString()],
-                    __('Yesterday') => [Carbon::yesterday()->toDateString(), Carbon::yesterday()->toDateString()],
-                    __('Last 7 Days') => [Carbon::today()->subDays(6)->toDateString(), Carbon::today()->toDateString()],
-                    __('Last 14 Days') => [Carbon::today()->subDays(13)->toDateString(), Carbon::today()->toDateString()],
-                    __('Last 30 Days') => [Carbon::today()->subDays(29)->toDateString(), Carbon::today()->toDateString()],
-                    __('This Month') => [Carbon::today()->startOfMonth()->toDateString(), Carbon::today()->endOfMonth()->toDateString()],
-                    __('Last Month') => [Carbon::today()->subMonth()->firstOfMonth()->toDateString(), Carbon::today()->subMonth()->lastOfMonth()->toDateString()],
-                ])->defaultRange([
-                    // last 30 days
-                    Carbon::today()->subDays(29)->toDateString(),
-                    Carbon::today()->toDateString()
-                ]);
+            if ($this->range){
+                $form->daterangepicker(["$this->id-start", "$this->id-end"], __('Period'))
+                    ->ranges($this->ranges ?? [
+                        __('Today') => [Carbon::today()->toDateString(), Carbon::today()->toDateString()],
+                        __('Yesterday') => [Carbon::yesterday()->toDateString(), Carbon::yesterday()->toDateString()],
+                        __('Last 7 Days') => [Carbon::today()->subDays(6)->toDateString(), Carbon::today()->toDateString()],
+                        __('Last 14 Days') => [Carbon::today()->subDays(13)->toDateString(), Carbon::today()->toDateString()],
+                        __('Last 30 Days') => [Carbon::today()->subDays(29)->toDateString(), Carbon::today()->toDateString()],
+                        __('This Month') => [Carbon::today()->startOfMonth()->toDateString(), Carbon::today()->endOfMonth()->toDateString()],
+                        __('Last Month') => [Carbon::today()->subMonth()->firstOfMonth()->toDateString(), Carbon::today()->subMonth()->lastOfMonth()->toDateString()],
+                    ])->defaultRange($this->defaultRange ?? [
+                        // last 30 days
+                        Carbon::today()->subDays(29)->toDateString(),
+                        Carbon::today()->toDateString()
+                    ]);
+            }else{
+                $form->daterangepicker("{$this->id}-date", __('Date'));
+            }
+
         }
         Admin::script($this->script());
 
@@ -118,19 +126,78 @@ class Chart extends Widget
 
     /**
      * @param $method
+     * @param bool $range
+     * @param array $ranges
+     * @param array $defaultRange
      * @return $this
      */
-    public function ajax($method): Chart
+    public function ajax($method, $range = false, $ranges = null, $defaultRange = null): Chart
     {
         $this->ajax = $method;
+        $this->range = $range;
+        $this->ranges = $ranges;
+        $this->defaultRange = $defaultRange;
         return $this;
+    }
+
+    protected function ajaxScript(): string
+    {
+        if ($this->range){
+            return $this->ajaxRangeScript();
+        }else{
+            return $this->ajaxDateScript();
+        }
     }
 
     /**
      * @return string
      */
-    protected function ajaxScript(): string
+    protected function ajaxDateScript(): string
     {
+        return <<<SCRIPT
+            $('.$this->id-date').ready(function() {
+                let date = $('.$this->id-date').val();
+                
+                $.ajax({
+                    url: '$this->ajax',
+                    data: {
+                        '_token' : LA.token,
+                        'date': date
+                    },
+                    dataType: 'json',
+                    success: function(data, textStatus, jqXHR){
+                        charts['$this->id'].data = data;
+                        charts['$this->id'].update();
+                    },
+                    fail: function(jqXHR, textStatus){
+                        console.error(textStatus);
+                    }
+                });
+    
+                $('.$this->id-date').on('apply.daterangepicker', function(ev, picker) {
+                    let date = $('.$this->id-date').val();
+                    
+                    $.ajax({
+                        url: '$this->ajax',
+                        data: {
+                            '_token' : LA.token,
+                            'date': date
+                        },
+                        dataType: 'json',
+                        success: function(data, textStatus, jqXHR){
+                            charts['$this->id'].data = data;
+                            charts['$this->id'].update();
+                        },
+                        fail: function(jqXHR, textStatus){
+                            console.error(textStatus);
+                        }
+                    });
+                });
+            });   
+SCRIPT;
+    }
+
+    private function ajaxRangeScript(): string{
         return <<<SCRIPT
             $('.$this->id-start_$this->id-end').ready(function() {
                 let range = $('.$this->id-start_$this->id-end').val().split(' - ');
